a2799fcd86cc41803c5374151f56bd7a91acc7d7
Indrajith <indrajithrnair.career@gemail.com>
2026-05-25 22:05:15 +0530

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/indrajithrnairaccion/puppy
+
+go 1.26.2
---
+++ puppy.go
@@ -0,0 +1,11 @@
+package puppy
+
+func bark() string{
+
+	return "Woof..!"
+}
+
+func barks() string {
+
+	return "Woof Woof..!!"
+}
